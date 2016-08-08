6d1217136a4d5586bb0f81d8d842b80df9c795d8
zbruno <zach@pocketderm.com>
2016-08-08 15:56:04 -0700

    Update example code

--- example/src/example.js
+++ example/src/example.js
@@ -4,9 +4,16 @@ var ReactImageGallery = require('react-image-gallery');
 
 var App = React.createClass({
 	render () {
+    const IMAGE_NAMES = ['cat', 'cats', 'chameleon', 'dog', 'ducks', 'goat', 'ostrich', 'pigeon', 'pigs', 'seagulls', 'wasp', 'yawn'];
+    const IMAGE_MAP = IMAGE_NAMES.map(img => ({
+      src: `./images/800-${img}.jpg`,
+      thumbnail: `./images/thumbnail-${img}.jpg`,
+      caption: img,
+    }));
+
 		return (
 			<div>
-				<ReactImageGallery />
+				<ReactImageGallery images={IMAGE_MAP} />
 			</div>
 		);
 	}
