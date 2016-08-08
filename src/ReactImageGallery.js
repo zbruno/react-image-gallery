ed26703baa1f68f8d8c380f90f341763779087a9
zbruno <zach@pocketderm.com>
2016-08-08 15:55:55 -0700

    Update components

--- src/ReactImageGallery.js
+++ src/ReactImageGallery.js
@@ -1,9 +1,151 @@
-var React = require('react');
+import React, { Component, PropTypes } from 'react';
+import ImageViewer from './ReactImageViewer';
 
-var ReactImageGallery = React.createClass({
-	render () {
-		return <div>react-image-gallery</div>;
-	}
-});
+class ImageGallery extends Component {
+  constructor() {
+    super();
 
-export default ReactImageGallery;
+    this.state = {
+      lightboxIsOpen: false,
+      currentImages: [],
+      compareMode: false,
+    };
+
+    this.closeLightbox = this.closeLightbox.bind(this);
+    this.gotoNext = this.gotoNext.bind(this);
+    this.gotoPrevious = this.gotoPrevious.bind(this);
+    this.openLightbox = this.openLightbox.bind(this);
+    this.onKeyDown = this.onKeyDown.bind(this);
+    this.onKeyUp = this.onKeyUp.bind(this);
+  }
+
+  componentWillMount() {
+    window.addEventListener('keydown', this.onKeyDown);
+    window.addEventListener('keyup', this.onKeyUp);
+  }
+
+  onKeyDown(e) {
+    if (e.keyCode === 16) {
+      this.setState({ compareMode: true });
+    }
+  }
+
+  onKeyUp(e) {
+    if (e.keyCode === 16) {
+      this.setState({ compareMode: false });
+
+      if (this.state.currentImages.length === 2) {
+        this.openLightbox(this.state.currentImages);
+      }
+    }
+  }
+
+  openLightbox(index) {
+    this.setState({
+      currentImages: index,
+      lightboxIsOpen: true,
+      compareMode: false,
+    });
+  }
+
+  closeLightbox() {
+    this.setState({
+      currentImages: [],
+      lightboxIsOpen: false,
+      compareMode: false,
+    });
+  }
+
+  gotoPrevious() {
+    if (this.state.currentImages.length === 1) {
+      this.setState({
+        currentImages: [this.state.currentImages[0] - 1],
+      });
+    }
+  }
+
+  gotoNext() {
+    if (this.state.currentImages.length === 1) {
+      this.setState({
+        currentImages: [this.state.currentImages[0] + 1],
+      });
+    }
+  }
+
+  handleClickTrigger(i, e) {
+    e.preventDefault();
+
+    if (this.state.compareMode) {
+      e.preventDefault();
+
+      if (this.state.currentImages.indexOf(i) > -1) {
+        this.state.currentImages.splice(this.state.currentImages.indexOf(i), 1);
+      } else if (this.state.currentImages.length < 2) {
+        this.state.currentImages.push(i);
+      }
+
+      this.setState({ currentImages: this.state.currentImages });
+    } else {
+      this.state.currentImages = [i];
+      this.setState({ currentImages: this.state.currentImages });
+      this.openLightbox(this.state.currentImages);
+    }
+  }
+
+  renderImageGallery() {
+    if (!this.props.images) return;
+
+    const visibilityClass = !this.state.compareMode ? 'image-gallery' : 'image-gallery opened';
+    const gallery = this.props.images.map((img, i) => {
+      const imageClassNames = this.state.currentImages.indexOf(i) > -1 ?
+        'image-gallery-image active' : 'image-gallery-image';
+      return (
+        <div key={i} className="image-gallery-image-container">
+          <img className={imageClassNames}
+            src={img.thumbnail}
+            onClick={(e) => this.handleClickTrigger(i, e)}
+            alt={img.caption}
+          />
+          <span className="image-gallery-image-caption">{img.caption}</span>
+        </div>
+      );
+    });
+
+    return (
+      <div className={visibilityClass}>
+        <h2 className="image-gallery-heading">Choose Photos to Compare</h2>
+        <p className="image-gallery-subheading">Click on photos to choose for comparison</p>
+        <div className="image-gallery-images-container">
+          {gallery}
+        </div>
+      </div>
+    );
+  }
+
+  render() {
+    console.log(this.state.currentImages);
+
+    return (
+      <div className="image-gallery-container">
+        {this.renderImageGallery()}
+        <ImageViewer
+          currentImages={this.state.currentImages}
+          images={this.props.images}
+          isOpen={this.state.lightboxIsOpen}
+          onClickPrev={this.gotoPrevious}
+          onClickNext={this.gotoNext}
+          onClickImage={this.handleClickImage}
+          onClose={this.closeLightbox}
+        />
+      </div>
+    );
+  }
+}
+
+ImageGallery.propTypes = {
+  heading: PropTypes.string,
+  images: PropTypes.array,
+  subheading: PropTypes.string,
+};
+
+export default ImageGallery;
---
+++ src/ReactImageViewer.js
@@ -0,0 +1,159 @@
+import React, { Component, PropTypes } from 'react';
+
+class ImageViewer extends Component {
+  constructor() {
+    super();
+
+    this.gotoNext = this.gotoNext.bind(this);
+    this.gotoPrevious = this.gotoPrevious.bind(this);
+    this.handleKeyboardInput = this.handleKeyboardInput.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.isOpen) {
+      window.addEventListener('keydown', this.handleKeyboardInput);
+    } else {
+      window.removeEventListener('keydown', this.handleKeyboardInput);
+    }
+  }
+
+  gotoNext(event) {
+    if (this.props.currentImages.length === 2 ||
+      this.props.currentImages[0] === (this.props.images.length - 1)) return;
+    if (event) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    this.props.onClickNext();
+  }
+
+  gotoPrevious(event) {
+    if (this.props.currentImages.length === 2 || this.props.currentImages[0] === 0) return;
+    if (event) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    this.props.onClickPrev();
+  }
+
+  handleKeyboardInput(e) {
+    switch (e.keyCode) {
+      case 37:
+        this.gotoPrev(event);
+        return true;
+      case 39:
+        this.gotoNext(event);
+        return true;
+      case 27:
+        this.props.onClose();
+        return true;
+      default:
+        return false;
+    }
+  }
+
+  renderArrowPrev() {
+    if (this.props.currentImages.length === 2 ||
+      this.props.currentImages[0] === 0) return null;
+
+    return (
+      <button className="image-viewer-icon-button prev-icon-button"
+        title="Previous (Left arrow key)"
+        type="button"
+        onClick={this.gotoPrev}
+        onTouchEnd={this.gotoPrev}>
+        <svg className="image-viewer-icon left-icon" viewBox="0 0 100 100">
+          <path d="M67.3,0.4l15,15L47.5,50.1l34.7,34.6l-15,15L17.7,50.1L67.3,0.4z"/>
+        </svg>
+      </button>
+    );
+  }
+
+  renderArrowNext() {
+    if (this.props.currentImages.length === 2 ||
+      this.props.currentImages[0] === (this.props.images.length - 1)) return null;
+
+    return (
+      <button className="image-viewer-icon-button next-icon-button"
+        title="Next (Right arrow key)"
+        type="button"
+        onClick={this.gotoNext}
+        onTouchEnd={this.gotoNext}>
+        <svg className="image-viewer-icon right-icon" viewBox="0 0 100 100">
+          <path d="M32.7,99.6l-15-15l34.7-34.7L17.7,15.3l15-15l49.6,49.6L32.7,99.6z"/>
+        </svg>
+      </button>
+    );
+  }
+
+  renderCloseButton() {
+    return (
+      <button className="image-viewer-icon-button close-icon-button"
+        title="Close (Esc)"
+        onClick={this.props.onClose}
+      >
+        <svg className="image-viewer-icon close-icon" viewBox="0 0 100 100">
+          <path d="M57.1,50l42.5,42.5l-7,7.1L50,57.1L7.5,99.6l-7.1-7.1L42.9,50L0.4,7.5l7.1-7.1L50,42.9L92.6,0.4l7,7.1L57.1,50z"/>
+        </svg>
+      </button>
+    );
+  }
+
+  renderImages() {
+    const { images, currentImages } = this.props;
+
+    if (!images || !images.length) return null;
+
+    const counter = currentImages.length === 1 ? (<div className="image-viewer-footer-count">{index + 1} of {images.length}</div>) : null;
+
+    return (currentImages.map((index) => {
+      return (
+        <figure key={`image ${index}`} className="image-viewer-figure">
+          <img className="image-viewer-image"
+            alt={images[index].caption}
+            key={index}
+            onClick={this.handleImageClick}
+            src={images[index].src}
+          />
+          <div className="image-viewer-footer">
+            {counter}
+            <figcaption className="image-viewer-footer-caption">{images[index].caption}</figcaption>
+          </div>
+        </figure>
+      );
+    }));
+  }
+
+  render() {
+    const classNames = this.props.isOpen ? 'image-viewer-container opened' : 'image-viewer-container';
+
+    return (
+      <div className={classNames}>
+        <div className="image-viewer-content">
+          {this.renderCloseButton()}
+          <div className="image-viewer-image-container">
+            {this.renderImages()}
+          </div>
+        </div>
+        {this.renderArrowPrev()}
+        {this.renderArrowNext()}
+      </div>
+    );
+  }
+}
+
+ImageViewer.propTypes = {
+  currentImages: PropTypes.array,
+  images: PropTypes.arrayOf(
+    PropTypes.shape({
+      src: PropTypes.string.isRequired,
+      caption: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  isOpen: PropTypes.bool,
+  onClickNext: PropTypes.func,
+  onClickPrev: PropTypes.func,
+  onClose: PropTypes.func.isRequired,
+};
+
+export default ImageViewer;
